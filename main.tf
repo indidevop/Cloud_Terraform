282df4c16aa878f1ee8c8d640ccab99d18ff3877
Amey Sawalkar <ameykishor@gmail.com>
2026-08-19 23:07:10 +0530

    Added public route table

--- main.tf
+++ main.tf
@@ -49,3 +49,16 @@ resource "aws_internet_gateway" "main" {
     Name = "terraform-igw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "terraform-public-route-table"
+  }
+}
