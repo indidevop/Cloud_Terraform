94722983c05c60d5ab1c78c503e984ccf6add8ee
Amey Sawalkar <ameykishor@gmail.com>
2026-08-24 05:19:27 +0530

    Done filtering

--- for_expression.tf
+++ for_expression.tf
@@ -4,9 +4,17 @@ locals {
   environment_names = [
         for env in local.environments_lst : upper(env)
     ]
+
+    nonprod_environments = [
+        for env in local.environments_lst : env if env != "prod"
+    ]
   
 }
 
 output "environment_names" {
   value = local.environment_names
 }
+
+output "nonprod"{
+   value = local.nonprod_environments
+}  
